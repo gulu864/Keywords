c94230e22229b6bf37f9223d1c970b3bf2e0b535
gulu864 <suva.kanta4u@gmail.com>
2024-11-22 18:55:50 +0530

    Add files via upload

---
+++ Keywords.py
@@ -0,0 +1,17 @@
+#break#
+for i in range(0,11,1):   
+    if i == 5:
+        break
+    print(i)
+print("----------")
+#pass#
+for i in range(0,11,1):    
+    if i == 5:
+        pass
+    print(i)
+print("----------")
+#continue#
+for j in range(0,11,1):
+    if j == 5:
+        continue
+    print(j)
---
+++ Project_while.py
@@ -0,0 +1,7 @@
+
+while True:
+    var1 = input("Do you want to loop to start?(Yes/No):")
+    
+    if var1 == "No":
+        break
+    print("You are in the while loop.")
